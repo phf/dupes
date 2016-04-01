f96c880b6a34b826d02c7675b75cd1c581cc7610
Peter Froehlich <peter.hans.froehlich@gmail.com>
2016-04-01 09:45:35 -0400

    Paranoid file comparison.

--- dupes.go
+++ dupes.go
@@ -1,13 +1,22 @@
 // dupes finds duplicate files in the given root directory
+//
+// TODO concurrent checksum/compare? multiple starting points?
+// library for other programs?
 package main
 
 import (
+	"bytes"
 	"crypto/sha256"
+	"flag"
 	"fmt"
 	"io"
 	"os"
 	"path/filepath"
+	"runtime"
 )
+
+var paranoid = flag.Bool("p", false, "paranoid byte-by-byte comparison")
+var goroutines = flag.Int("j", runtime.NumCPU(), "number of goroutines")
 
 // hashes maps from digests to paths
 var hashes = make(map[string]string)
@@ -26,6 +35,54 @@ var dupes int64
 
 // wasted counts the space (in bytes) occupied by duplicates
 var wasted int64
+
+// identical does a byte-by-byte comparison of the files with the
+// given paths
+func identical(pa, pb string) (bool, error) {
+	const bufferSize = 4096
+
+	a, err := os.Open(pa)
+	if err != nil {
+		return false, err
+	}
+	defer a.Close()
+	b, err := os.Open(pb)
+	if err != nil {
+		return false, err
+	}
+	defer b.Close()
+
+	// TODO: move these out to avoid reallocating them? lazy make?
+	ba := make([]byte, bufferSize)
+	bb := make([]byte, bufferSize)
+
+	for {
+		la, erra := a.Read(ba)
+		lb, errb := b.Read(bb)
+
+		if erra != nil || errb != nil {
+			if erra == io.EOF && errb == io.EOF {
+				return true, nil
+			}
+			if erra != nil {
+				return false, erra
+			}
+			if errb != nil {
+				return false, errb
+			}
+		}
+
+		if la != lb { // TODO: short read always at end of file?
+			return false, nil
+		}
+
+		if !bytes.Equal(ba, bb) {
+			return false, nil
+		}
+	}
+
+	return true, nil
+}
 
 // checksum calculates a hash digest for the file with the given path
 func checksum(path string) (string, error) {
@@ -83,6 +140,17 @@ func check(path string, info os.FileInfo, err error) error {
 		return nil
 	}
 
+	if *paranoid {
+		same, err := identical(path, dupe)
+		if err != nil {
+			return err
+		}
+		if !same {
+			fmt.Printf("cool: %s sha256-collides with %s!\n", path, dupe)
+			return nil
+		}
+	}
+
 	fmt.Printf("%s\n%s\n\n", path, dupe)
 	dupes++
 	wasted += size
@@ -91,12 +159,13 @@ func check(path string, info os.FileInfo, err error) error {
 }
 
 func main() {
-	if len(os.Args) < 2 {
+	flag.Parse()
+	if len(flag.Args()) < 1 {
 		fmt.Println("error: need an argument")
 		os.Exit(1)
 	}
 
-	filepath.Walk(os.Args[1], check)
+	filepath.Walk(flag.Arg(0), check)
 
 	if len(hashes) > 0 {
 		fmt.Printf("%d files examined, %d duplicates found, %d bytes wasted\n", files, dupes, wasted)
